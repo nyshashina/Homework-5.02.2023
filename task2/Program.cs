5ce0e4a35f82293952d2fa3010a3d19b01978a3b
nyshashina <nyshashina@gmail.com>
2023-02-14 23:06:18 +0300

    task2 was done

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу масштабирования фигуры
+void ScalingFigure(string strcoords, double mult)
+{
+    char[] separators = new char[] { ' ', ',', '(', ')' };
+    string[] subs = strcoords.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    double[] doublecoords = new double[subs.Length];
+    for(int i = 0; i < subs.Length; i++)
+    {
+        doublecoords[i] = Convert.ToDouble(subs[i]);
+        doublecoords[i] = doublecoords[i] * mult;
+    }
+    Console.WriteLine("");
+    Console.Write($"Новые координаты фигуры: ");
+    for(int i = 0; i < doublecoords.Length; i++)
+    {
+        Console.Write($"({doublecoords[i]},{doublecoords[i+1]}) ");
+        i++;
+    }
+}
+Console.WriteLine("Введите координаты фигуры одной строкой");
+string coords1 = Console.ReadLine () ?? "0";
+Console.WriteLine("Введите коэффициент масштабирования");
+double mult1 = double.Parse(Console.ReadLine() ?? "0");
+ScalingFigure(coords1, mult1);
